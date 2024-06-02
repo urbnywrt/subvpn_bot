8e98d1a0810a13455b6409a270ed63c6be517e01
urbnywrt <jacora.apb@gmail.com>
2024-06-02 23:53:01 +0300

    Add logging

--- bot.py
+++ bot.py
@@ -10,7 +10,11 @@ from marzpy import Marzban
 from marzpy.api.user import User
 from telebot import types
 from telebot.util import user_link
+import logging
 
+# Configure the logger
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
 
 target_channel = int(os.environ['TARGET_CHANNEL'])
 check_cooldown = int(os.environ['CHECK_COOLDOWN'])
@@ -22,9 +26,7 @@ bot_token = os.environ['BOT_TOKEN']
 bot = AsyncTeleBot(bot_token)
 
 
-
-
-@bot.message_handler(commands=['vpn','start'])
+@bot.message_handler(commands=['vpn', 'start'])
 async def vpn_message(message):
     # user = bot.get_chat_member(chat_id=mob5ter_channel_id, user_id=message.from_user.id)
     # print('check', user)
@@ -55,11 +57,6 @@ async def vpn_message(message):
     await bot.send_message(message.chat.id, text=welcome_message, reply_markup=keyboardmain, parse_mode='HTML')
 
 
-
-
-
-
-
 async def check_user_in_channel(user_id):
     try:
         user = await bot.get_chat_member(chat_id=target_channel, user_id=user_id)
@@ -67,12 +64,13 @@ async def check_user_in_channel(user_id):
     except telebot.asyncio_helper.ApiTelegramException:
         return False
     except Exception as e:
-        print(e)
+        logger.warning(e)
         return False
+
 
 async def check_user_marzban(tg_id):
     try:
-        panel = Marzban(panel_username,panel_pass,panel_address)
+        panel = Marzban(panel_username, panel_pass, panel_address)
         mytoken = await panel.get_token()
         user = await panel.get_user(f"SUB_{tg_id}", mytoken)
         return user
@@ -86,62 +84,63 @@ async def add_marzban_user(tg_id, tg_name):
         mytoken = await panel.get_token()
         sub_date = datetime.datetime.today() + timedelta(days=31)
         user = await panel.add_user(user=User(username=f"SUB_{tg_id}",
-                                        note=f"{tg_name}",
-                                        proxies={
-                                            "vless": {
-                                                "flow": "xtls-rprx-vision"
-                                            }
-                                        },
-                                        data_limit=536870912000,
-                                        expire=sub_date.timestamp(),
-                                        data_limit_reset_strategy="no_reset",
-                                        status="active",
-                                        inbounds={
-                                            "vless": [
-                                                "VLESS TCP REALITY"
-                                            ]
-                                        }), token=mytoken)
+                                              note=f"{tg_name}",
+                                              proxies={
+                                                  "vless": {
+                                                      "flow": "xtls-rprx-vision"
+                                                  }
+                                              },
+                                              data_limit=536870912000,
+                                              expire=sub_date.timestamp(),
+                                              data_limit_reset_strategy="no_reset",
+                                              status="active",
+                                              inbounds={
+                                                  "vless": [
+                                                      "VLESS TCP REALITY"
+                                                  ]
+                                              }), token=mytoken)
         return user
     except Exception as e:
-        print(e)
+        logger.warning(e)
         return False
 
 
-
 async def check_tg_and_recharge():
-    print("CHECK FOR EXPIRED USERS")
+    logger.info("CHECK FOR EXPIRED USERS")
     panel = Marzban(panel_username, panel_pass, panel_address)
     mytoken = await panel.get_token()
     users = await panel.get_all_users(token=mytoken)
     for item in users:
         if item.status == 'expired' and "SUB_" in item.username:
-            #print(item.username + " " + item.status)
+            # print(item.username + " " + item.status)
             tg_user_id = str(item.username).replace("SUB_", "")
             user = await check_user_in_channel(user_id=int(tg_user_id))
             if user:
-                #print(user.user.full_name, user.status)
+                # print(user.user.full_name, user.status)
                 if user.status in ['member', 'administrator', 'creator']:
                     sub_date = datetime.today() + timedelta(days=32)
                     await panel.modify_user(user_username=f"SUB_{tg_user_id}", user=User(username=f"SUB_{tg_user_id}",
-                                                                                   note=f"{user.user.full_name}",
-                                                                                   proxies={
-                                                                                       "vless": {
-                                                                                           "flow": "xtls-rprx-vision"
-                                                                                       }
-                                                                                   },
-                                                                                   data_limit=536870912000,
-                                                                                   expire=sub_date.timestamp(),
-                                                                                   data_limit_reset_strategy="no_reset",
-                                                                                   status="active",
-                                                                                   inbounds={
-                                                                                       "vless": [
-                                                                                           "VLESS TCP REALITY"
-                                                                                       ]
-                                                                                   }), token=mytoken)
+                                                                                         note=f"{user.user.full_name}",
+                                                                                         proxies={
+                                                                                             "vless": {
+                                                                                                 "flow": "xtls-rprx-vision"
+                                                                                             }
+                                                                                         },
+                                                                                         data_limit=536870912000,
+                                                                                         expire=sub_date.timestamp(),
+                                                                                         data_limit_reset_strategy="no_reset",
+                                                                                         status="active",
+                                                                                         inbounds={
+                                                                                             "vless": [
+                                                                                                 "VLESS TCP REALITY"
+                                                                                             ]
+                                                                                         }), token=mytoken)
                     await panel.reset_user_traffic(user_username=f"SUB_{tg_user_id}", token=mytoken)
-                    print(f"recharge {tg_user_id}")
+                    logger.info(f"recharge {tg_user_id}")
             else:
-                print(f"user {tg_user_id} not found on channel")
+                logger.info(f"user {tg_user_id} not found on channel")
+
+
 async def main():
     await asyncio.gather(bot.polling(), schedule_task())
 
@@ -154,5 +153,6 @@ async def schedule_task():
 
     while True:
         await asyncio.sleep(2)
+
 
 asyncio.run(main())
